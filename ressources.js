893aa579e814cb3e8f6c4dac134138bb7f54d4f1
davidwanga <>
2022-05-19 07:46:24 +0000

    création du tableau pour le stockage des questions et reponses

---
+++ ressources.js
@@ -0,0 +1,206 @@
+let questions = [
+
+    {
+
+        numb : 1,
+        question : "Quel est l'extension d'un fichier javascript?" ,
+        answer : ".js" ,
+        options : [
+            ".ts",
+            ".jsx",
+            ".js",
+            ".js"
+
+        ]
+    },
+    {
+
+        numb : 2,
+        question : "Quel est le moteur javascript pour google chrome?" ,
+        answer : "V8 javascript engine" ,
+        options : [
+            " V12 javascript engine",
+            "Chakra javascript Engine",
+            "V8 javascript engine",
+            "Edge"
+
+        ]
+    },
+    {
+
+        numb : 3,
+        question : "Comment déclare t-on une fonction?" ,
+        answer : "function mafonction(x){...}" ,
+        options : [
+            "def __init__{...}",
+            "def ma_fonction(){...}",
+            "public static mafonction(){...}",
+            "function mafonction(){... }"
+
+        ]
+    },
+    {
+
+        numb : 4,
+        question : "Comment déclare t-on une variable?" ,
+        answer : "let maVar" ,
+        options : [
+            "int idStudent = 3",
+            "string maVar = 'david' ",
+            "let maVar",
+            "$maVar"
+
+        ]
+    },
+    {
+
+        numb : 5,
+        question : "Comment créer un objet en javascript?" ,
+        answer : "let personne = {...}" ,
+        options : [
+            "int idStudent = 3",
+            "string maVar = 'david' ",
+            "let maVar",
+            "$maVar"
+
+        ]
+    },
+    {
+
+        numb : 6,
+        question : " Laquelle de librairie suivante n'est pas du javascript" ,
+        answer : "springBoot" ,
+        options : [
+            "Vuejs",
+            "nextjs",
+            "react native",
+            "springBoot"
+
+        ]
+    },
+    {
+
+        numb : 7,
+        question : "Parmi les avantages de javascript suivant suivant l'un est faux " ,
+        answer : "Javascript est dépendant de la plateforme" ,
+        options : [
+            "Javascript est un langage coté client",
+            "Javascript est un langage facile à apprendre",
+            "Javascript est dépendant de la plateforme",
+            "facile à déboguer"
+
+        ]
+    },
+    {
+
+        numb : 8,
+        question : "Qu'est ce que le DOM?" ,
+        answer : "document objet model" ,
+        options : [
+            "département d'outre mer",
+            "document d'ordre maritime",
+            "document open model",
+            "document objet model"
+
+        ]
+    },
+    {
+
+        numb : 9,
+        question : "A quoi sert le mot clé this? " ,
+        answer : "le mot-clé « this » fait  réference à l'objet actuelle" ,
+        options : [
+            "le mot-clé « this » fait réference à la fonction",
+            "le mot-clé « this » fait réference au DOM ",
+            "le mot-clé « this » fait réference à la variable",
+            "le mot-clé « this » fait  réference à l'objet actuelle"
+
+        ]
+    },
+    {
+
+        numb : 10,
+        question : "Comment trouvez-vous le nombre avec la plus grande valeur de « a » et « b »?" ,
+        answer : "Math.max(a,b)" ,
+        options : [
+            "Math.ceil(a, b)",
+            "Math.max(a, b)",
+            "ceil(a, b)",
+            "top(a, b)"
+
+        ]
+    },
+
+    {
+
+        numb : 11,
+        question : "Comment arrondir le nombre 3.12 à un nombre entier plus proche?" ,
+        answer : "Math.round(3.12)" ,
+        options : [
+            "Math.round(3.12)",
+            "Math.rnd(3.12)",
+            "float(3.12)",
+            "Math.float(3.12)"
+
+        ]
+    },
+    {
+
+        numb : 12,
+        question : "Quelle est la syntaxe correcte de la boucle while?" ,
+        answer : "while(x <= 5)" ,
+        options : [
+            " while i = 1 to 5;",
+            "while i <= 5; i++;",
+            "while (i = 0; i<=5; i++);",
+            "while(x<=5);"
+
+        ]
+    },
+    {
+
+        numb : 13,
+        question : "Que renvoie la question suivante isNaN(‘222’)?" ,
+        answer : "false" ,
+        options : [
+            "true",
+            "false",
+          
+
+        ]
+    },
+    {
+
+        numb : 14,
+        question : "Donnez la valeur attendu de l'opération suivante (true | | false) &&( !true)" ,
+        answer : "false" ,
+        options : [
+            "true",
+            "false",
+          
+
+        ]
+    },
+    {
+
+        numb : 15,
+        question : "javascript est un langage fortement typé typé Vrai ou Faux?" ,
+        answer : "Faux" ,
+        options : [
+            "Vrai",
+            "Faux",
+          
+
+        ]
+    },
+
+
+    
+    
+    
+
+
+
+
+
+]
